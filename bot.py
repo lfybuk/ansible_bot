7321e32f22d4809bb0ef64a94d97f1c42a18d40f
lfybuk <67377504+lfybuk@users.noreply.github.com>
2024-05-18 21:10:55 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -116,7 +116,7 @@ def get_phone_numbers_handler(update, context):
         update.message.reply_text('Список номеров телефонов пуст.')
 
 def get_repl_log(update, context):
-    output = ssh_exec_command('cat /var/log/postgresql/postgresql-14-main.log | grep repl')
+    output = ssh_exec_command('cat /var/log/postgresql/postgresql-14-main.log | grep repl | tail -n 15')
     update.message.reply_text(output)
 
 def find_email(update, context):
